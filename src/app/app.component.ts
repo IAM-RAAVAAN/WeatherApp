17bdf57e3ee33b27e293ba5f6082a49d85e92882
swaraj <swarajlokesh@gmail.com>
2024-10-02 18:11:32 -0700

    backend for the searching cities

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,13 @@ import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common'; // Import CommonModule
 import { WeatherData } from './modules/weather.model';
 import {WeatherService} from './services/weather.service';
+import { FormsModule } from '@angular/forms';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,CommonModule],
+  imports: [RouterOutlet,CommonModule,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -17,9 +18,20 @@ import {WeatherService} from './services/weather.service';
 export class AppComponent implements OnInit {
   constructor(private weatherService:WeatherService){}
   
+  cityName: string = 'Seattle'
   weatherData?: WeatherData
   ngOnInit(): void {
-    this.weatherService.getWeatherData('Seattle')
+    this.getWeatherData(this.cityName);
+  }
+
+  onSubmit(): void {
+      this.getWeatherData(this.cityName);
+      this.cityName = "";
+
+  }
+
+  private getWeatherData(cityName:string){
+      this.weatherService.getWeatherData(cityName)
     .subscribe({
       next: (responce: any) =>{
         this.weatherData = responce;
--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -3,6 +3,7 @@ import { HttpClient,HttpHeaders, HttpParams } from '@angular/common/http';
 import { environmentVariables } from '../app.config';
 import { WeatherData } from '../modules/weather.model';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -18,8 +19,12 @@ export class WeatherService {
       headers: new HttpHeaders().set(environmentVariables.xRapidapiHost,environmentVariables.xRapidapiHostVal)
       .set(environmentVariables.xRapidapiKey,environmentVariables.xRapidapiKeyVal),
       params: new HttpParams().set("q",cityName)
-      //.set("u","c").set('format','json')
-    })
+
+    }).pipe(
+      tap(data => {
+        console.log('Weather data received:', data); // Log the received data
+      })
+      );
   }
 }
 
